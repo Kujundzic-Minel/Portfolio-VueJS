38413ef0756b5cdacf127647464f6d41004db067
Minel <Minel@example.com>
2024-03-11 09:31:37 +0100

    Fix button "contact me" + responsive header & mySkills

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,91 @@
 <template>
-    <nav class="m-7 sticky top-[30px] HeaderAnimation z-50 ">
-              <div class="flex justify-between items-center p-2 bg-[#353232] bg-opacity-50  rounded-full border border-[#60A337]">
-        <div class="flex items-center">
-          <img src="/image/MK-LOGO.svg" alt="Minel Kujundzic Logo" class="lg:h-14 lg:px-7 min-[320px]:h-7 min-[320px]:px-3">
+  <nav class="m-4 sticky top-[30px] z-50">
+    <div
+      class="flex justify-between items-center p-2 bg-[#353232] bg-opacity-50 rounded-full border border-[#60A337]"
+    >
+      <div class="flex items-center">
+        <img
+          src="/image/MK-LOGO.svg"
+          alt="Minel Kujundzic Logo"
+          class="h-8 sm:h-10 lg:h-14 px-2 sm:px-4 lg:px-7"
+        />
+      </div>
+      <ul
+        class="hidden md:flex items-center space-x-2 md:space-x-4 z-40 text-white text-xs sm:text-sm lg:text-xl"
+      >
+        <li class="list-none">
+          <a href="/">Home</a>
+        </li>
+        <li class="list-none">Skills</li>
+        <li class="list-none">Project</li>
+      </ul>
+      <div class="flex items-center">
+        <div
+          class="hidden md:block p-1 sm:p-2 lg:p-4 border rounded-full border-[#60A337] bg-gradient-to-r from-[#52773B] to-[#364d27] gradient-hover"
+        >
+          <button
+            class="text-white font-bold text-xs sm:text-sm lg:text-xl px-2 sm:px-4 lg:px-6"
+          >
+            Contact me
+          </button>
         </div>
-        <ul class="hidden md:flex items-center space-x-4 z-40">
-          <li class="list-none text-white lg:text-xl sm:text-lg min-[320px]:text-sm">
-            <a href="/">Home</a>
-          </li>
-          <li class="list-none text-white lg:text-xl sm:text-lg min-[320px]:text-sm">Skills</li>
-          <li class="list-none text-white lg:text-xl sm:text-lg min-[320px]:text-sm">Project</li>
-        </ul>
-        <div class="flex items-center">
-          <div class="p-2 border rounded-full border-[#60A337] bg-gradient-to-r from-[#52773B] to-[#364d27] gradient-hover min-[320px]:p-1 sm:p-4">
-            <button class="text-white font-bold lg:text-xl sm:text-lg lg:px-6 min-[320px]:text-sm min-[320px]:px-2 sm:px-5">Contact me</button>
-          </div>
-          <div class="md:hidden ml-4">
-            <button class="text-white focus:outline-none" @click="isOpen = !isOpen">
-              <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 6H20M4 12H20M4 18H20" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-            </button>
-          </div>
+        <div class="md:hidden ml-2 sm:ml-4">
+          <button
+            class="text-white focus:outline-none"
+            @click="isOpen = !isOpen"
+          >
+            <svg
+              class="w-6 h-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 6H20M4 12H20M4 18H20"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
         </div>
       </div>
-      <div v-show="isOpen" class="md:hidden">
-        <ul class="bg-[#353232] bg-opacity-25 rounded-lg p-4 mt-2 space-y-2">
-          <li class="list-none text-white text-lg">
-            <a href="/">Home</a>
-          </li>
-          <li class="list-none text-white text-lg">Skills</li>
-          <li class="list-none text-white text-lg">Project</li>
-          <div class="p-2 border rounded-full border-[#60A337] bg-gradient-to-r from-[#52773B] to-[#364d27] gradient-hover">
-            <button class="text-white font-bold text-lg px-6">Contact me</button>
-          </div>
-        </ul>
-      </div>
-    </nav>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const isOpen = ref(false);
-  </script>
-  
-  <style>
-  .HeaderAnimation {
-    animation: HeaderAnimation 1s ease-out forwards;
+    </div>
+    <div v-show="isOpen" class="md:hidden">
+      <ul
+        class="bg-[#353232] bg-opacity-25 rounded-lg p-4 mt-2 space-y-2 text-white text-sm"
+      >
+        <li class="list-none">
+          <a href="/">Home</a>
+        </li>
+        <li class="list-none">Skills</li>
+        <li class="list-none">Project</li>
+        <div
+          class="p-2 border rounded-full border-[#60A337] bg-gradient-to-r from-[#52773B] to-[#364d27] gradient-hover mt-4"
+        >
+          <button class="text-white font-bold text-lg px-6">Contact me</button>
+        </div>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const isOpen = ref(false);
+</script>
+
+<style>
+.HeaderAnimation {
+  animation: HeaderAnimation 1s ease-out forwards;
+}
+@keyframes HeaderAnimation {
+  0% {
+    transform: translateY(-200%);
   }
-  
-  @keyframes HeaderAnimation {
-    0% {
-      transform: translateY(-200%);
-    }
-    100% {
-      transform: translateY(0);
-    }
+  100% {
+    transform: translateY(0);
   }
-  </style>
+}
+</style>
--- src/components/mySkills.vue
+++ src/components/mySkills.vue
@@ -1,35 +1,40 @@
 <script setup lang="ts">
 </script>
+
 <template>
-    <main>
+  <main>
     <div class="w-full flex justify-center">
-      <div class="flex justify-center mt-8">
+      <div class="flex flex-wrap justify-center mt-8">
         <img
           src="/image/Adobe_Creative_Cloud_rainbow_icon 1.svg"
           alt=""
-          class="w-36
-           m-7
-            mx-14"
+          class="w-24 sm:w-36 m-4 mx-6 sm:mx-14"
         />
-        <img src="/image/Figma-logo 1.svg" alt="" class="w-24 m-7 mx-14" />
-        <img src="/image/React-icon 1.svg" alt="" class="w-36 m-7 mx-14" />
+        <img
+          src="/image/Figma-logo 1.svg"
+          alt=""
+          class="w-16 sm:w-24 m-4 mx-6 sm:mx-14"
+        />
+        <img
+          src="/image/React-icon 1.svg"
+          alt=""
+          class="w-24 sm:w-36 m-4 mx-6 sm:mx-14"
+        />
       </div>
     </div>
     <div class="w-full flex justify-center">
-      <div class="flex justify-center">
+      <div class="flex flex-wrap justify-center">
         <img
           src="/image/typescript-logo-B29A3F462D-seeklogo 1.svg"
           alt=""
-          class="w-36
-           m-7 mx-14"
+          class="w-24 sm:w-36 m-4 mx-6 sm:mx-14"
         />
         <img
           src="/image/tailwind-css 1.svg"
           alt=""
-          class="w-36
-         m-7 mx-14"
+          class="w-24 sm:w-36 m-4 mx-6 sm:mx-14"
         />
       </div>
     </div>
-    </main>
+  </main>
 </template>
