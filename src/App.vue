e495363b2eb1799a868a3bc29c00ea52420c6e33
Minel <Minel@example.com>
2024-03-12 14:47:13 +0100

    curseur personnalisé

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <script setup lang="ts">
-	import { RouterLink, RouterView } from 'vue-router'
-  import Header from './components/Header.vue';
-  import Footer from './components/Footer.vue';
+import { onMounted, onUnmounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
+onMounted(() => {
+  const mouse = document.querySelector("#mouse");
   
+  const updateMousePosition = (e) => {
+    mouse.style.top = e.clientY + 'px';
+    mouse.style.left = e.clientX + 'px';
+  };
+
+  window.addEventListener('mousemove', updateMousePosition);
+
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', updateMousePosition);
+  });
+});
 </script>
 
 <template>
-  <Header />
-
-  <suspense>
-    <RouterView />
-  </suspense>
-
-  <Footer />
+    <Header />
+    <div id="mouse">
+    </div>
+    <suspense>
+      <RouterView />
+    </suspense>
+    <Footer />
 </template>
+
+<style scoped>
+#mouse {
+  position: fixed; /* Changé de 'absolute' à 'fixed' pour un positionnement global */
+  top: 0; /* Initialisé à 0; il sera mis à jour par JavaScript */
+  left: 0; /* Initialisé à 0; il sera mis à jour par JavaScript */
+  width: 20px; /* Taille réduite pour l'exemple */
+  height: 20px; /* Taille réduite pour l'exemple */
+  border-radius: 50%;
+  border: 2px solid rgb(200, 247, 211);
+  transform: translate(-50%, -50%);
+  pointer-events: none; /* Assurez-vous que le curseur ne gêne pas les clics */
+  z-index: 999;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,18 +3,12 @@
 import Hero from '@/components/Hero.vue';
 import mySkills from '@/components/mySkills.vue';
 import skillBar from '@/components/skillBar.vue';
-import cards from '@/components/cards.vue';
-import testsupa from '@/components/testsupa.vue';
 import cardsSupa from '@/components/cardsSupa.vue';
 </script>
 <template>
   <Hero />
   <skillBar />
   <mySkills />
-  <skillBar />
-  <cards />
-  <skillBar />
-  <testsupa />
   <skillBar />
   <cardsSupa />
 </template>
