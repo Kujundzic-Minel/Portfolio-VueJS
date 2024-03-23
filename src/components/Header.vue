bea281e830daff9499cc3be9cd681bc694a8c91d
Minel <Minel@example.com>
2024-03-23 23:45:24 +0100

    fix navigation header mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,11 +7,17 @@ const isOpen = ref(false)
 const scrollToSkills = () => {
   const skillsSection = document.getElementById('skills-section')
   skillsSection.scrollIntoView({ behavior: 'smooth' })
+  closeMenu()
 }
 
 const scrollToProjects = () => {
   const projectsSection = document.getElementById('projects-section')
   projectsSection.scrollIntoView({ behavior: 'smooth' })
+  closeMenu()
+}
+
+const closeMenu = () => {
+  isOpen.value = false
 }
 </script>
 
@@ -77,24 +83,20 @@ const scrollToProjects = () => {
     <div v-show="isOpen" class="md:hidden">
       <ul class="bg-[#353232] bg-opacity-25 rounded-lg p-4 mt-2 space-y-2 text-white text-sm">
         <li class="list-none hover:font-semibold">
-          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/" @click="closeMenu">Home</RouterLink>
         </li>
-        <li class="list-none hover:font-semibold" @click="scrollToSkills">
-          Skills
-        </li>
-        <li class="list-none hover:font-semibold" @click="scrollToProjects">
-          Project
-        </li>
+        <li class="list-none hover:font-semibold" @click="scrollToSkills">Skills</li>
+        <li class="list-none hover:font-semibold" @click="scrollToProjects">Project</li>
         <li class="list-none hover:font-semibold">
-          <RouterLink to="/whoiam">Who I am</RouterLink>
+          <RouterLink to="/whoiam" @click="closeMenu">Who I am</RouterLink>
         </li>
-        <RouterLink to="../pages/contact.vue"
-          ><div
+        <RouterLink to="../pages/contact.vue" @click="closeMenu">
+          <div
             class="p-2 border rounded-full border-[#60A337] bg-gradient-to-r from-[#52773B] to-[#364d27] gradient-hover text-center mt-4"
           >
             <button class="text-white font-bold text-lg px-6 contact-btn">Contact me</button>
-          </div></RouterLink
-        >
+          </div>
+        </RouterLink>
       </ul>
     </div>
   </nav>
