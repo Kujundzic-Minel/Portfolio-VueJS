75e4bc724409d1ddf67279b1d5707d776eced52f
Minel <Minel@example.com>
2024-03-24 00:10:19 +0100

    début page whoiam

--- src/pages/whoiam.vue
+++ src/pages/whoiam.vue
@@ -1,6 +1,25 @@
-<script setup lang="ts"></script>
 <template>
-  <div>
-    <h1>Who I am</h1>
+  <div class="flex flex-col items-center justify-center min-h-screen p-4">
+    <div class="shadow-lg rounded-lg overflow-hidden md:flex bg-[#333333]">
+      <div class="bg-cover bg-center md:w-1/3" style="background-image: url('/public/image/MK-PP-Cartoon.png');">
+      </div>
+      <div class="p-8">
+        <h2 class="text-2xl font-semibold mb-4">Minel Kujundzic</h2>
+        <p class="mb-4">Étudiant à MMI Montbéliard en spécialité développement web, avec un projet de poursuivre un master en intelligence artificielle ou en informatique.</p>
+        
+        <!-- Icônes des réseaux sociaux -->
+        <div class="flex justify-start mt-4">
+          <a href="https://linkedin.com/in/minel-kujundzic" class="mr-6 text-gray-600 hover:text-gray-700">
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.8 0-5 2.2-5 5v14c0 2.8 2.2 5 5 5h14c2.8 0 5-2.2 5-5v-14c0-2.8-2.2-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.3c-.9 0-1.5-.6-1.5-1.4s.6-1.4 1.5-1.4 1.5.6 1.5 1.4-.7 1.4-1.5 1.4zm13.5 12.3h-3v-5.6c0-3.4-4-3.1-4 0v5.6h-3v-11h3v1.5c1.4-2.6 7-2.8 7 2.5v7z"/></svg>
+          </a>
+          <a href="https://instagram.com/votreprofil" class="text-gray-600 hover:text-gray-900">
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><rect fill="none" height="24" width="24"/><path d="M16,8.41L11.41,13L16,17.59L14.59,19l-6-6l6-6L16,8.41z"/></svg>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
