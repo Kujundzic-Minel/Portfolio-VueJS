54343f836dcc8c144c47d375ac37db1f693f1ccb
Minel <Minel@example.com>
2024-03-25 16:46:14 +0100

    technos dynamique

--- src/components/mySkills.vue
+++ src/components/mySkills.vue
@@ -1,27 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { supabase } from '@/lib/supabaseClient'
+
+const technos = ref([])
+
+onMounted(async () => {
+  try {
+    const { data: technosData } = await supabase
+      .from('technos')
+      .select('*')
+
+    technos.value = technosData
+  } catch (error) {
+    console.error('Erreur lors de la récupération des technologies :', error)
+  }
+})
+</script>
 
 <template>
-  <main>
-    <div class="w-full flex justify-center">
-      <div class="flex flex-wrap justify-center mt-8">
-        <img
-          src="/image/Adobe_Creative_Cloud_rainbow_icon 1.svg"
-          alt=""
-          class="w-24 sm:w-36 m-4 mx-6 sm:mx-14"
-        />
-        <img src="/image/Figma-logo 1.svg" alt="" class="w-16 sm:w-24 m-4 mx-6 sm:mx-14" />
-        <img src="/image/React-icon 1.svg" alt="" class="w-24 sm:w-36 m-4 mx-6 sm:mx-14" />
+  <main class="w-full flex flex-col items-center">
+    <div v-if="technos.length === 0" class="mt-8">
+      Aucune techno spécifiée
+    </div>
+    <div v-else class="flex flex-wrap justify-center mt-8">
+      <div
+        v-for="techno in technos"
+        :key="techno.id"
+        class="m-4 mx-6 sm:mx-14"
+      >
+        <img :src="techno.icon_techno" alt="Logo de la technologie" class="w-24 sm:w-36 object-cover" />
       </div>
     </div>
-    <div class="w-full flex justify-center pb-7">
-      <div class="flex flex-wrap justify-center">
-        <img
-          src="/image/typescript-logo-B29A3F462D-seeklogo 1.svg"
-          alt=""
-          class="w-24 sm:w-36 m-4 mx-6 sm:mx-14"
-        />
-        <img src="/image/tailwind-css 1.svg" alt="" class="w-24 sm:w-36 m-4 mx-6 sm:mx-14" />
-      </div>
+    <div v-if="technos.length > 0" class="w-full flex justify-center pb-7">
+      <!-- Vous pouvez ajouter d'autres éléments ici si nécessaire -->
     </div>
   </main>
 </template>
